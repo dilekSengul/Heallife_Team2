de53908b5cccb12a57c7707b7f6e0cb27a674a5c
Serpil Sönmez <serpilsonmez16@gmail.com>
2024-11-22 23:08:35 +0300

    serpil

--- src/test/java/StepDefinitions/HomePageStep.java
+++ src/test/java/StepDefinitions/HomePageStep.java
@@ -15,44 +15,7 @@ import java.sql.Driver;
 public class HomePageStep {
 
     private static final Logger logger = LogManager.getLogger(  HomePageStep.class);
-<<<<<<< HEAD
 
-    // [Header] - [Home] bölümünün elementleri
-
-    @FindBy(xpath = "(//*[text()='Home'])[1]")
-    public WebElement Home;
-
-    @FindBy(xpath = "//a[@class='navbar-brand logo']//img")
-    public WebElement HLlogo;
-
-    @FindBy(xpath = "//*[text()='Bu siteye ulaşılamıyor']")
-    public WebElement errorMessage;
-
-    // [Header] - [Appointment] bölümünün elementleri
-
-    @FindBy (xpath = "(//*[text()='Appointment'])[1]")
-    public WebElement Appointment;
-
-    // [Header] - [Login] bölümünün elementleri
-
-    @FindBy (xpath = "//*[text()='Login']")
-    public WebElement Login;
-
-    // [Header] - [Contant Us] bölümünün elementleri
-
-    @FindBy (xpath = "(//*[text()='Contact Us'])[1]")
-    public WebElement ContactUs;
-
-    // [Header] - [About Us] bölümünün elementleri
-
-    @FindBy (xpath = "(//*[text()='About Us '])[1]")
-    public WebElement AboutUs;
-
-
-
-
-
-    // [Footer] - [Social Media] bölümünün elementleri
 
 
     @Given("Kullanıcı projectUrl adresini ziyaret eder")
@@ -133,14 +96,8 @@ public class HomePageStep {
 
 
 
-    // [Footer] - [Communication] bölümünün elementleri
 
-    // [Footer] - [Copyright] bölümünün elementleri
 
-    // [Home] - [Methods] buraya eklenecek
-=======
-/*asdf*/
->>>>>>> main
 
 
 
